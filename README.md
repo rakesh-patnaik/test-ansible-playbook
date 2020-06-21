77ca90b5ebf06237e4ecc1e41950728a5e3995e1
Rakesh Patnaik <patsrakesh@gmail.com>
2020-06-21 08:10:54 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Test commands
+
+```ansible-playbook playbooks/deploy_application.yml -e "host_group_name=localhost"```
---
+++ download_roles.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ansible-galaxy install -r roles/requirements.yml --ignore-errors --roles-path roles/ --force
+
